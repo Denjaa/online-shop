510ebefb050b1754146f76f3787783f9d5cfbc9f
denis.morozov <denis.morozov>
2022-01-30 17:27:21 +0000

    change

--- Jenkinsfile
+++ Jenkinsfile
@@ -25,7 +25,7 @@ pipeline {
         stage('Build') {
                     steps {
                         // Gradle clean the previous builds and build new project
-                        sh ('./gradlew clean -x build')
+                        sh ('./gradlew clean build')
                     }
                 }
 
--- src/main/java/com/shop/Shop/ShopApplication.java
+++ src/main/java/com/shop/Shop/ShopApplication.java
@@ -1,13 +1,24 @@
 package com.shop.Shop;
 
+import com.shop.Shop.entity.Customers;
+import com.shop.Shop.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner sampleData(CustomerRepository repository) {
+		return (args) -> {
+			repository.save(new Customers(1, "denis", "King Street", "morozov@hot.ee", 21));
+		};
 	}
 
 }
