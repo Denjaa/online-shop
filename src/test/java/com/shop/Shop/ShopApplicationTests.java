294b9aa7928d05811091ba553079ee26741edd5a
denis.morozov <denis.morozov>
2022-01-31 18:44:53 +0000

    change

--- src/test/java/com/shop/Shop/ShopApplicationTests.java
+++ src/test/java/com/shop/Shop/ShopApplicationTests.java
@@ -10,8 +10,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -21,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class ShopApplicationTests {
 
-	private String generated_tokenizer = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY0MzY2OTk5MCwiaWF0IjoxNjQzNjMzOTkwfQ.X7z4NEgWHhLfCxQ9Nc9rm8v2dyKmGlkKA6OgZ87vaC8";
+	private String generated_tokenizer = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY0MzY4OTk0NiwiaWF0IjoxNjQzNjUzOTQ2fQ.mKMIXEK-qR5rdho4ECZrvhAESlvhT3D-7AXHxDXePyA";
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -57,8 +59,8 @@ class ShopApplicationTests {
 	public void deleteCustomer() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.delete("/customers/delete/5")
 						.accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, generated_tokenizer))
-						.andExpect(status().isOk())
-						.andReturn();
+				.andExpect(status().isOk())
+				.andReturn();
 	}
 
 	@Test
@@ -94,6 +96,56 @@ class ShopApplicationTests {
 						.accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, generated_tokenizer))
 				.andExpect(status().isOk())
 				.andReturn();
+	}
+
+	@Test
+	public void getCustomerValues() throws Exception {
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/customers/getById/1")
+						.accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, generated_tokenizer))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String stringResult = result.getResponse().getContentAsString();
+		assertEquals("{\"id\":1,\"name\":\"Denis\",\"surname\":\"Morozov\",\"address\":\"Dunville Green, Navan\",\"email\":\"denis@student.ait.ie\",\"productId\":1}", stringResult);
+	}
+
+	@Test
+	public void addOrUpdateCustomerValues() throws Exception {
+		String customer = "{\"id\":3,\"name\":\"Tester\",\"surname\":\"Tester Surname\",\"address\":\"Testing Sample Address\",\"email\":\"test@test.com\",\"product_id\":100}";
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/customers/addOrUpdate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(customer)
+						.accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, generated_tokenizer))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String stringResult = result.getResponse().getContentAsString();
+		assertEquals("{\"id\":3,\"name\":\"Tester\",\"surname\":\"Tester Surname\",\"address\":\"Testing Sample Address\",\"email\":\"test@test.com\",\"productId\":0}", stringResult);
+	}
+
+	@Test
+	public void getProductsValues() throws Exception {
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/products/getById/1")
+						.accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, generated_tokenizer))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String stringResult = result.getResponse().getContentAsString();
+		assertEquals("{\"id\":1,\"name\":\"Magic House \",\"brand\":\"L.O.L\",\"availability\":\"yes\"}", stringResult);
+	}
+
+	@Test
+	public void addOrUpdateProductValues() throws Exception {
+		String customer = "{\"id\":3,\"name\":\"Tester\",\"brand\":\"Tester Surname\",\"availability\":\"Testing\"}";
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/products/addOrUpdate")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(customer)
+						.accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, generated_tokenizer))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String stringResult = result.getResponse().getContentAsString();
+		assertEquals("{\"id\":3,\"name\":\"Tester\",\"brand\":\"Tester Surname\",\"availability\":\"Testing\"}", stringResult);
 	}
 
 
